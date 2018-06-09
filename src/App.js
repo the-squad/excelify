04c403c1a2d1da9f68decc6fa2411cc7058eed2e
Muhammad Tarek <muhammad.tarek@vezeeta.com>
2018-06-09 17:26:05 +0200

    Integrate hot-app-reload for faster dev env

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { hot } from 'react-hot-loader';
 
 const App = () => (
   <div className="App">
@@ -11,4 +12,4 @@ const App = () => (
   </div>
 );
 
-export default App;
+export default hot(module)(App);
