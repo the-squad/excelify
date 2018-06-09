5e9ee9aab5576f3b820d6050070a15b2599eb375
Muhammad Tarek <muhammad.tarek@vezeeta.com>
2018-06-09 21:51:50 +0200

    Integrate Header and SetGlobalFont

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, injectGlobal } from 'styled-components';
 import { hot } from 'react-hot-loader';
 
 import theme from './base/Theme';
 import Upload from './views/Upload';
+
+import Header from './components/header/Header';
+import SetGlobalFont from './components/text/SetGlobalFont';
+
+/* eslint-disable no-unused-expressions */
+injectGlobal`
+  body {
+    margin: 0;
+  }
+`;
+/* eslint-enable no-unused-expressions */
 
 const routes = [
   {
@@ -21,19 +32,30 @@ const routes = [
   },
 ];
 
-const App = () => (
-  <ThemeProvider theme={theme}>
-    <React.Fragment>
-      {routes.map(route => (
-        <Route
-          key={route.path}
-          exact={route.path === '/'}
-          path={route.path}
-          component={route.component}
-        />
-      ))}
-    </React.Fragment>
-  </ThemeProvider>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    SetGlobalFont();
+  }
+
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <React.Fragment>
+          <Header />
+          {routes.map(route => (
+            <Route
+              key={route.path}
+              exact={route.path === '/'}
+              path={route.path}
+              component={route.component}
+            />
+          ))}
+        </React.Fragment>
+      </ThemeProvider>
+    );
+  }
+}
 
 export default hot(module)(App);
