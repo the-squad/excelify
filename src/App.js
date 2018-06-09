6f6b54cbed539d2113f5915656ea3b7c6385aa5e
Muhammad Tarek <muhammad.tarek@vezeeta.com>
2018-06-09 21:13:50 +0200

    Init routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import { hot } from 'react-hot-loader';
 
+import theme from './base/Theme';
+import Upload from './views/Upload';
+
+const routes = [
+  {
+    path: '/',
+    component: () => <div>Home</div>,
+  },
+  {
+    path: '/upload',
+    component: Upload,
+  },
+  {
+    path: '/edit',
+    component: () => <div>edit</div>,
+  },
+];
+
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
+  <ThemeProvider theme={theme}>
+    <React.Fragment>
+      {routes.map(route => (
+        <Route
+          key={route.path}
+          exact={route.path === '/'}
+          path={route.path}
+          component={route.component}
+        />
+      ))}
+    </React.Fragment>
+  </ThemeProvider>
 );
 
 export default hot(module)(App);
---
+++ src/views/Upload.js
@@ -0,0 +1,11 @@
+// @flow
+import React, { Component } from 'react';
+
+class Upload extends Component {
+  upload = () => {};
+  render() {
+    return <div>Upload</div>;
+  }
+}
+
+export default Upload;
