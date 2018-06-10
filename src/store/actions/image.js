511223bfe3aaad1e0c61115fa36299440385ef69
Muhammad Tarek <muhammad.tarek@vezeeta.com>
2018-06-10 02:25:40 +0200

    Add flow type

--- src/store/actions/image.js
+++ src/store/actions/image.js
@@ -1,8 +1,9 @@
+// @flow
 export const IMAGE = {
   SET: 'SET_IMAGE',
 };
 
-export const saveImage = image => ({
+export const saveImage = (image: String): Object => ({
   type: IMAGE.SET,
   image,
 });
