01b89a533fce397080f890ba6537f0ff9ab98afb
Muhammad Tarek <muhammad.tarek@vezeeta.com>
2018-06-10 02:07:37 +0200

    Connect store and update routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { ThemeProvider, injectGlobal } from 'styled-components';
 import { hot } from 'react-hot-loader';
+import { Provider } from 'react-redux';
+
+import store from './store';
 
 import theme from './base/Theme';
-import Upload from './views/Upload';
+import Upload from './views/upload';
 
 import Header from './components/header/Header';
 import SetGlobalFont from './components/text/SetGlobalFont';
+import IconsStore from './components/icon/IconsStore';
+
+import Icons from './assets/selection.json';
 
 /* eslint-disable no-unused-expressions */
 injectGlobal`
@@ -19,16 +25,8 @@ injectGlobal`
 
 const routes = [
   {
-    path: '/',
-    component: () => <div>Home</div>,
-  },
-  {
     path: '/upload',
     component: Upload,
-  },
-  {
-    path: '/edit',
-    component: () => <div>edit</div>,
   },
 ];
 
@@ -37,22 +35,25 @@ class App extends React.Component {
     super(props);
 
     SetGlobalFont();
+    IconsStore.setIcons(Icons);
   }
 
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <React.Fragment>
-          <Header />
-          {routes.map(route => (
-            <Route
-              key={route.path}
-              exact={route.path === '/'}
-              path={route.path}
-              component={route.component}
-            />
-          ))}
-        </React.Fragment>
+        <Provider store={store}>
+          <React.Fragment>
+            <Header />
+            {routes.map(route => (
+              <Route
+                key={route.path}
+                exact={route.path === '/'}
+                path={route.path}
+                component={route.component}
+              />
+            ))}
+          </React.Fragment>
+        </Provider>
       </ThemeProvider>
     );
   }
