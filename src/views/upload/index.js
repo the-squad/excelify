cf941819d0500c2139ddbbd861729cf5232cb494
Muhammad Tarek <muhammad.tarek@vezeeta.com>
2018-06-12 00:14:31 +0200

    Contain upload process to Modal

--- src/views/upload/index.js
+++ src/views/upload/index.js
@@ -1,16 +1,20 @@
+// @flow
 import React from 'react';
+import styled from 'styled-components';
 import { Route, BrowserRouter } from 'react-router-dom';
 import { Box } from 'grid-styled';
 
-import ViewContainer from '../ViewContainer';
 import Upload from './Upload';
 import Crop from './Crop';
 import Edit from './Edit';
 
+import Modal from '../../components/modal/Modal';
 import Progress from '../../components/progress/Progress';
 import Text from '../../components/text/Text';
+import ImageField from '../../components/imageField/ImageField';
 
-import { FONT_TYPES, FONT_WEIGHTS } from '../../base/Typography';
+import { FONT_TYPES } from '../../base/Typography';
+import { COLORS, COLORS_VALUES } from '../../base/Colors';
 
 const routes = [
   {
@@ -42,40 +46,77 @@ const steps = [
   },
 ];
 
-const UploadContainer = () => (
-  <ViewContainer>
-    <Box mb={3} />
-    <Text type={FONT_TYPES.TITLE} fontWeight={FONT_WEIGHTS.SEMI_BOLD}>
-      Upload an image you want to convert
-    </Text>
-    <Box mb={2} />
-    <Text>Go through this step to make sure to upload the most optimized image</Text>
-    <Box mb={4} />
-    <Progress
-      steps={steps}
-      currentStep={routes.findIndex(
-        route =>
-          route.path ===
-          `/${window.location.pathname
-            .split('/')
-            .slice(-1)
-            .pop()}`,
-      )}
-    />
-    <Box mb={4} />
-    <BrowserRouter basename="upload">
+const Separator = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: ${COLORS_VALUES[COLORS.SEPARATOR]};
+`;
+
+type State = {
+  currentStep: number,
+};
+
+class UploadContainer extends React.Component<null, State> {
+  state = {
+    currentStep: 0,
+  };
+
+  getImageFieldRef = () => this.imageField;
+  imageField: ?Object;
+
+  nextStep = () => {
+    this.setState(prevState => ({
+      currentStep: prevState.currentStep + 1,
+    }));
+  };
+
+  previousStep = () => {
+    this.setState(prevState => ({
+      currentStep: prevState.currentStep - 1,
+    }));
+  };
+
+  render() {
+    const { currentStep } = this.state;
+
+    return (
       <React.Fragment>
-        {routes.map(route => (
-          <Route
-            key={route.path}
-            exact={route.path === '/'}
-            path={route.path}
-            component={route.component}
-          />
-        ))}
+        <ImageField
+          ref={imageField => {
+            this.imageField = imageField;
+          }}
+        />
+        <Modal>
+          <Box p={3}>
+            <Text type={FONT_TYPES.SUBHEADING}>Upload an image you want to convert</Text>
+          </Box>
+          <Separator />
+          <Box p={3}>
+            <Progress steps={steps} currentStep={currentStep} />
+            <Box mb={4} />
+            <BrowserRouter basename="upload">
+              <React.Fragment>
+                {routes.map(route => (
+                  <Route
+                    key={route.path}
+                    exact={route.path === '/'}
+                    path={route.path}
+                    component={() => (
+                      <route.component
+                        getImageFieldRef={this.getImageFieldRef}
+                        nextStep={this.nextStep}
+                        previousStep={this.previousStep}
+                      />
+                    )}
+                  />
+                ))}
+              </React.Fragment>
+            </BrowserRouter>
+          </Box>
+        </Modal>
       </React.Fragment>
-    </BrowserRouter>
-  </ViewContainer>
-);
+    );
+  }
+}
 
 export default UploadContainer;
