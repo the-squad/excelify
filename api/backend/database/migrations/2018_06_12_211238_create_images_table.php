da2cbf3c6611a87ff645297f3daf0a39ea5b17a2
Muhammed Kamal <muhammedkmal3@gmail.com>
2018-06-13 00:32:22 +0200

    Api laravel
    Api to integrate with front and python

---
+++ api/backend/database/migrations/2018_06_12_211238_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('path');
+            $table->string('title')->nullable();
+            $table->json('table');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
