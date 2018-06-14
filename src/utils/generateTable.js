289a6e0c452b4d05d0114f368bbcf6144779e980
Muhammad Tarek <muhammad.tarek@vezeeta.com>
2018-06-14 22:27:11 +0200

    Fix a flow error

--- src/utils/generateTable.js
+++ src/utils/generateTable.js
@@ -5,7 +5,7 @@ import Lodash from 'lodash';
 const alphabetic = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 // Generates columns
-export const generateColumns = (): Array<any> =>
+export const generateColumns = (): Array<Object> =>
   Array.from(alphabetic).map(
     (char: string): Object => ({
       name: char,
@@ -15,7 +15,7 @@ export const generateColumns = (): Array<any> =>
     }),
   );
 
-export const generateEmptyColumn = (rowIndex: number): Array<any> =>
+export const generateEmptyColumn = (rowIndex: number): Array<Object> =>
   Array.from(alphabetic).map(
     (_: any, index: number): Object => ({
       name: '',
@@ -24,7 +24,7 @@ export const generateEmptyColumn = (rowIndex: number): Array<any> =>
     }),
   );
 
-export const generateRow = (row: Array<any>, rowIndex: number): Array<Object> =>
+export const generateRow = (row: Array<string | number>, rowIndex: number): Array<Object> =>
   Array.from(alphabetic).map(
     (_: any, index: number): Object => ({
       name: row[index],
@@ -34,31 +34,32 @@ export const generateRow = (row: Array<any>, rowIndex: number): Array<Object> =>
   );
 
 export const removeRowCount = (row: Array<Object>): Array<Object> => {
-  const rowCopy = Lodash.cloneDeep(row);
+  const rowCopy: Array<Object> = Lodash.cloneDeep(row);
   rowCopy.shift();
   return rowCopy;
 };
 
-export const getPureTable = (table: Array<Array<any>>): Array<Array<any>> => {
-  const cleanTable = [];
-  const exportTable = [];
+export const getPureTable = (table: Array<Array<Object>>): Array<Array<string | number>> => {
+  const cleanTable: Array<Array<Object>> = [];
+  const exportTable: Array<Array<string | number>> = [];
 
   // Remove the first row and row's number cell's
   for (let rowCounter = 1; rowCounter < 25; rowCounter += 1) {
+    // $FlowFixMe
     cleanTable.push(removeRowCount(table[rowCounter - 1]));
   }
 
   // Get cell's value
-  cleanTable.forEach((row: Array<any>) => {
-    const rowItems = row.map((column: Array<Object>) => column.value);
+  cleanTable.forEach((row: Array<Object>) => {
+    const rowItems = row.map((column: Object) => column.value);
     exportTable.push(rowItems);
   });
 
   return exportTable;
 };
 
-export const generateTable = (rows: Array<Array<any>>) => {
-  const table = [];
+export const generateTable = (rows: Array<Array<string | number>>): Array<Array<Object>> => {
+  const table: Array<Array<Object>> = [];
   table.push(generateColumns());
   for (let rowsCounter = 0; rowsCounter <= 25; rowsCounter += 1) {
     if (rowsCounter < rows.length) {
