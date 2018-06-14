c0dcbd52c4c387042a747cf48e7452cd41fb6b83
Muhammad Amin <mohamedamin.fcih@gmail.com>
2018-06-14 17:16:16 +0200

    ModelGenerator class added

---
+++ api/Code/ModelGenerator.py
@@ -0,0 +1,108 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.utils import np_utils
+from tensorflow.python.keras._impl.keras.optimizers import Adam
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import MaxPooling2D, Dropout
+from tensorflow.python.keras.layers import Conv2D, Dense, Flatten
+
+class ModelGenrator:
+    def __init__(self):
+        self.img_size = 28
+        self.img_size_flat = self.img_size * self.img_size
+        self.img_shape = (self.img_size, self.img_size)
+        self.img_shape_keras = (self.img_size, self.img_size, 1)
+        self.num_channels = 1
+        self.num_classes = 47
+        self.train_path = "emnist-balanced-train.csv"
+        self.test_path = "emnist-balanced-test.csv"
+
+    def rotate(self,img):
+        flipped = np.fliplr(img.reshape(self.img_size, self.img_size))
+        return np.rot90(flipped).reshape(self.img_size, self.img_size, 1)
+
+    def preprocess_data(self,dataset):
+        X = dataset.iloc[:, 1:]
+        y = dataset.iloc[:, 0]
+        X = np.asarray(X).reshape(dataset.shape[0], self.img_size, self.img_size, 1).astype('float32')
+        X /= 255
+        for i in range(len(X)):
+            X[i] = self.rotate(X[i])
+        true_classes = y
+        y = np_utils.to_categorical(y, 47)
+        return X, y, true_classes
+
+    def plot_accuracy(self,history):
+        # summarize history for accuracy
+        plt.plot(history.history['acc'])
+        plt.plot(history.history['val_acc'])
+        plt.title('model accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.show()
+
+    def plot_loss(self,history):
+        # summarize history for loss
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.show()
+
+    def generate(self):
+        train = pd.read_csv(self.train_path)
+        test = pd.read_csv(self.test_path)
+        X_train, y_train, _ = self.preprocess_data(train)
+        X_test, y_test, _ = self.preprocess_data(test)
+
+        model = Sequential()
+        model.add(Conv2D(kernel_size=5,
+                        strides=1,
+                        filters=32,
+                        activation='relu',
+                        name='conv_layer1',
+                        input_shape=self.img_shape_keras,
+                        padding='same'))
+
+        model.add(MaxPooling2D(pool_size=2, strides=2))
+        model.add(Dropout(0.5))
+
+        model.add(Conv2D(kernel_size=5,
+                        strides=1,
+                        filters=64,
+                        activation='relu',
+                        name='conv_layer2',
+                        padding='same'))
+        model.add(MaxPooling2D(pool_size=2, strides=2))
+        model.add(Dropout(0.5))
+
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(self.num_classes, activation='softmax'))
+
+        optimizer = Adam(lr=0.0001)
+        model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+        early_stopping_monitor = EarlyStopping(patience=2,monitor='val_acc')
+        checkpoint = ModelCheckpoint(filepath='model.h5',
+                                            monitor='val_acc',
+                                            verbose=1,
+                                            save_best_only=True,
+                                            mode='max')
+        history = model.fit(X_train, y_train, validation_split=0.3, batch_size=200, epochs=50, callbacks=[early_stopping_monitor,checkpoint],
+                verbose=2)
+
+        model.summary()
+        score = model.evaluate(X_test, y_test, verbose=0)
+        print('Test loss:', score[0])
+        print('Test accuracy:', score[1])
+
+        self.plot_accuracy(history)
+        self.plot_loss(history)
+
+
