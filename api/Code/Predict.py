61117f22b9455e941fe59b221b41e493d6ef314e
Muhammad Amin <mohamedamin.fcih@gmail.com>
2018-06-14 18:14:46 +0200

    Predict class added

--- api/Code/Predict.py
+++ api/Code/Predict.py
@@ -3,8 +3,8 @@ from keras.models import load_model
 class Predict:
 
     def __init__(self):
-        self.mappings_path = "emnist-balanced-mapping.txt"
-        self.model = load_model('model.h5')
+        self.mappings_path = "C:\\Users\\mohamed\\Desktop\\Final\\emnist-balanced-mapping.txt"
+        self.model = load_model("C:\\Users\\mohamed\\Documents\\GitHub\\graduation-project\\api\\Code\\Model\\model.h5")
 
     def load_char_mappings(self,mapping_path):
         """
@@ -21,7 +21,7 @@ class Predict:
     def predict(self,image):
         mappings = self.load_char_mappings(self.mappings_path)
         img = image.reshape(1, 28, 28, 1)
-        img = image.astype('float64')
+        img = img.astype('float64')
         img /= 10
         y_pred_int = self.model.predict_classes(img)
         return chr(mappings.get(y_pred_int[0]))
