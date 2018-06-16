7c41511e926d87d98b06d2d2bfdf757446115175
Muhammad Tarek <muhammad.tarek@vezeeta.com>
2018-06-16 18:58:45 +0200

    Revert removing methods from GenerateTable

--- src/utils/_test_/GenerateTable.test.js
+++ src/utils/_test_/GenerateTable.test.js
@@ -3,7 +3,7 @@ import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
 import 'jest-styled-components';
 
-import { generateTable } from '../GenerateTable';
+import { generateTable, removeRowCount } from '../GenerateTable';
 
 configure({ adapter: new Adapter() });
 
@@ -14,5 +14,11 @@ describe('GenerateTable', () => {
 
     expect(table.length).toEqual(27);
     expect(table[0].length).toEqual(27);
+  });
+
+  it('should remove row number cell', () => {
+    let row = [0, 1, 3];
+    row = removeRowCount(row);
+    expect(row).toEqual([1, 3]);
   });
 });
