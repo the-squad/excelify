68dc43b0245caea81a2fbc7afecea57f925a2d8c
Muhammad Tarek <muhammad.tarek@vezeeta.com>
2018-06-16 19:03:12 +0200

    Integrate save/load sheet listeners

--- src/store/reducers/sheets.js
+++ src/store/reducers/sheets.js
@@ -1,24 +1,60 @@
 // @flow
-import { SHEETS } from '../actions/sheets';
 
-import Sheets from '../../fakeData.json';
+import _ from 'lodash';
+
+import { SHEETS } from '../actions/sheets';
+import { IMAGE } from '../actions/image';
+
+import { generateTable } from '../../utils/GenerateTable';
+import { renderer } from '../../components/renderer/Renderer';
 
 const initialState = {
-  sheets: Sheets.sheets.map(sheet => ({
-    name: sheet.name,
-    date: sheet.date,
-    image: sheet.image,
-    sheet: sheet.sheet,
-  })),
+  sheets: new Map(),
+  sheetsState: renderer.empty(),
 };
 
 export default (state: Object = initialState, { type, ...payload }: Object) => {
   switch (type) {
     case SHEETS.SET: {
-      const sheets = state.sheets.unshift(payload.sheets);
+      const newSheets = state.sheets;
+      newSheets.set(newSheets.size, generateTable(payload.sheets));
+
       return {
         ...state,
-        sheets,
+        sheets: newSheets,
+      };
+    }
+
+    case SHEETS.UPDATE: {
+      const newSheets = _.cloneDeep(state.sheets);
+      const selectedSheet = state.sheets.get(payload.index);
+      const grid = selectedSheet.sheet.map(row => [...row]);
+
+      // Looping through cell and update changed cell value
+      payload.changes.forEach(({ row, col, value }) => {
+        grid[row][col] = { ...grid[row][col], value, name: value };
+      });
+
+      // Update changed sheet within sheets array
+      newSheets.set(payload.index, {
+        ...selectedSheet,
+        sheet: grid,
+      });
+
+      return {
+        ...state,
+        sheets: newSheets,
+      };
+    }
+
+    case IMAGE.CONVERTED: {
+      const newSheets = state.sheets;
+      newSheets.set(state.sheets.size, payload.table);
+
+      return {
+        ...state,
+        sheets: newSheets,
+        sheetsState: renderer.success(),
       };
     }
 
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,6 +1,9 @@
 import { fork } from 'redux-saga/effects';
 
-const sagas = [];
+import image from './image';
+import sheets from './sheets';
+
+const sagas = [image, sheets];
 
 export default function* rootSaga() {
   yield sagas.map(saga => fork(saga));
