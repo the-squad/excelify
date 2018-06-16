9271807992e906f790bc0c93f1c3b00e22df7e56
Muhammad Tarek <muhammad.tarek@vezeeta.com>
2018-06-16 19:02:52 +0200

    Integrate convertImage API

--- src/store/actions/image.js
+++ src/store/actions/image.js
@@ -3,11 +3,14 @@ export const IMAGE = {
   SET: 'SET_IMAGE',
   SET_CROPPED: 'SET_CROPPED_IMAGE',
   CONVERT: 'CONVERT_IMAGE',
+  CONVERTED: 'CONVERT_IMAGE_SUCCEEDED',
+  FAILED: 'CONVERT_IMAGE_FAILED',
 };
 
-export const saveImage = (image: String): Object => ({
+export const saveImage = (image: String, imageName: String): Object => ({
   type: IMAGE.SET,
   image,
+  imageName,
 });
 
 export const saveCroppedImage = (image: String, points: Object): Object => ({
@@ -16,7 +19,17 @@ export const saveCroppedImage = (image: String, points: Object): Object => ({
   points,
 });
 
-export const convertImage = (image: string) => ({
+export const convertImage = (image: string, imageName: String) => ({
   type: IMAGE.CONVERT,
   image,
+  imageName,
+});
+
+export const convertImageSucceeded = (table: Object) => ({
+  type: IMAGE.CONVERTED,
+  table,
+});
+
+export const convertImageFailed = () => ({
+  type: IMAGE.FAILED,
 });
