3354b845844a612bb5f4e5f2ec88422aa26815fd
Muhammed Kamal <muhammedkmal3@gmail.com>
2018-06-17 18:50:15 +0200

    start main

--- api/Code/Mainn.py
+++ api/Code/Mainn.py
@@ -1,1 +1,13 @@
-class Main:
+import json
+import cv2
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../..")
+from api.Code.ImageParser import ImageParser
+
+if __name__ == "__main__":
+    img = cv2.imread(sys.argv[1])
+    sheet = ImageParser()
+    sheet = sheet.parse(img)
+    print(json.dumps(sheet))
