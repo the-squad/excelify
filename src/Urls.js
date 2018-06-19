8bd2c361324a29147bc3d102fee549ae689d7a6d
Muhammad Tarek <muhammad.tarek@vezeeta.com>
2018-06-19 02:11:30 +0200

    Add missing URLs

--- src/Urls.js
+++ src/Urls.js
@@ -1,7 +1,8 @@
-// TODO: Change this
-const baseUrl = 'http://graduation.dev.com:8000';
+const baseUrl = process.env.REACT_APP_API_HOST || '127.0.0.1:8000';
 
 export default {
   history: `${baseUrl}/api/history`,
   parse: `${baseUrl}/api/parse`,
+  login: `${baseUrl}/api/login`,
+  signup: `${baseUrl}/api/register`,
 };
